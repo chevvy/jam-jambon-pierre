75cb4764117efeac8e9123bfed7dfc1f1ed8f2a7
Jean fr <jean.fr.main@gmail.com>
2024-11-08 15:55:24 -0500

    JFlvl

--- Player/PlayerInput.cs
+++ Player/PlayerInput.cs
@@ -13,20 +13,20 @@ public enum InputAction
 public partial class PlayerInput
 {
   public static Dictionary<PlayerID, string> PlayerTagByID = new(){
-    {PlayerID.P1, "p1"},
-    {PlayerID.P2, "p2"},
-    {PlayerID.P3, "p3"},
-    {PlayerID.P4, "p4"},
-    {PlayerID.P5, "p5"}
+	{PlayerID.P1, "p1"},
+	{PlayerID.P2, "p2"},
+	{PlayerID.P3, "p3"},
+	{PlayerID.P4, "p4"},
+	{PlayerID.P5, "p5"}
   };
 
   private static readonly Dictionary<InputAction, string> InputByName = new() {
-    { InputAction.MoveLeft, "_move_left" },
-    { InputAction.MoveRight, "_move_right" },
-    { InputAction.MoveUp, "_move_up"},
-    { InputAction.MoveDown, "_move_down"},
-    { InputAction.Jump, "_jump" },
-    { InputAction.Start, "_start" }
+	{ InputAction.MoveLeft, "_move_left" },
+	{ InputAction.MoveRight, "_move_right" },
+	{ InputAction.MoveUp, "_move_up"},
+	{ InputAction.MoveDown, "_move_down"},
+	{ InputAction.Jump, "_jump" },
+	{ InputAction.Start, "_start" }
   };
   private Dictionary<InputAction, string> _inputs;
 
@@ -34,16 +34,16 @@ public partial class PlayerInput
 
   public PlayerInput(PlayerID id)
   {
-    _id = id;
-    var playerName = PlayerTagByID[id];
-    (InputAction action, string actionName) getInputInfo(InputAction action) => (action, actionName: playerName + InputByName[action]);
+	_id = id;
+	var playerName = PlayerTagByID[id];
+	(InputAction action, string actionName) getInputInfo(InputAction action) => (action, actionName: playerName + InputByName[action]);
 
-    _inputs = new();
-    foreach (var inputWithName in InputByName.ToList())
-    {
-      var (action, actionName) = getInputInfo(inputWithName.Key);
-      _inputs[action] = actionName;
-    }
+	_inputs = new();
+	foreach (var inputWithName in InputByName.ToList())
+	{
+	  var (action, actionName) = getInputInfo(inputWithName.Key);
+	  _inputs[action] = actionName;
+	}
   }
 
   public string GetInputKey(InputAction action) => _inputs[action];
