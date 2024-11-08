2aac85dc625a4e95723a824a68e8d81fef7fc27f
Alix Lelièvre <lelievre.alix@gmail.com>
2024-11-08 00:10:37 -0500

    WIP

---
+++ Scenes/MainScene.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class MainScene : Node
+{
+	private Dictionary<int, PackedScene> SceneStore;
+	private Node SceneAnchorNode;
+
+	public override void _Ready()
+	{
+		SceneStore = new Dictionary<int, PackedScene> {
+			{ Scenes.Menu.SceneId, ResourceLoader.Load<PackedScene>(Scenes.Menu.ScenePath) ?? throw new Exception($"{Scenes.Menu.ScenePath} not found") },
+			{ Scenes.Game.SceneId, ResourceLoader.Load<PackedScene>(Scenes.Game.ScenePath) ?? throw new Exception($"{Scenes.Game.ScenePath} not found") },
+			{ Scenes.Credit.SceneId, ResourceLoader.Load<PackedScene>(Scenes.Credit.ScenePath) ?? throw new Exception($"{Scenes.Credit.ScenePath} not found") }
+		};
+
+		SceneAnchorNode = GetNode<Node>("SceneAnchor") ?? throw new Exception("SceneAnchor (Node) not found");
+
+		Signals.Instance.SceneRequested += OnSceneRequested;
+		Signals.Instance.EmitSignal(Signals.SignalName.SceneRequested, Scenes.Menu.SceneId);
+	}
+
+    public override void _Input(InputEvent @event)
+    {
+		if 		(Input.IsActionJustPressed("load_scene_0")) Signals.Instance.EmitSignal(Signals.SignalName.SceneRequested, Scenes.Menu.SceneId);
+		else if (Input.IsActionJustPressed("load_scene_1")) Signals.Instance.EmitSignal(Signals.SignalName.SceneRequested, Scenes.Game.SceneId);
+		else if (Input.IsActionJustPressed("load_scene_2")) Signals.Instance.EmitSignal(Signals.SignalName.SceneRequested, Scenes.Credit.SceneId);
+    }
+
+	public void LoadScene(int sceneId)
+	{
+		if (SceneAnchorNode.GetChildCount() > 0)
+			SceneAnchorNode.RemoveChild(SceneAnchorNode.GetChild(0));
+		
+		SceneAnchorNode.AddChild(SceneStore[sceneId].Instantiate());
+	}
+	
+	public void OnSceneRequested(int sceneId)
+	{
+		LoadScene(sceneId);
+	}
+}
---
+++ Signals.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+/**
+Should only define signals which are used throughout multiple scenes.
+If you KNOW the signal will only be used within a specific scene, then define it within that scene as you usually would.
+
+To DEFINE a signal, add its definition in this file:
+	[Signal] public delegate void GameSceneEndStateReachedEventHandler(bool wasSuccess);
+
+TO EMIT a signal, use the following code:
+	Signals.Instance.EmitSignal(Signals.SignalName.GameSceneEndStateReached, ...args);
+
+TO CONNECT to a signal:
+	Signals.Instance.GameSceneEndStateReached += OnGameSceneEndStateReached;
+
+TO DISCONNECT from a signal:
+	Signals.Instance.GameSceneEndStateReached -= OnGameSceneEndStateReached;
+*/
+
+public partial class Signals : Node
+{
+	[Signal] public delegate void SceneRequestedEventHandler(int sceneId);
+
+	public static Signals Instance { get; private set; }
+	public override void _EnterTree() => Instance = this;
+}
